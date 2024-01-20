f32b0497d9d249fe0fb0fe8e97cf4e83401b5d1d
Mateusz Wejman <mateusz.piotr.wejman@gmail.com>
2024-01-20 17:42:51 +0100

    Removed conversation.ex

--- lib/scam_web/live/conversation_live/index.ex
+++ lib/scam_web/live/conversation_live/index.ex
@@ -2,29 +2,16 @@ defmodule ScamWeb.ConversationLive.Index do
   use ScamWeb, :live_view
 
   alias Scam.AddictionCheck
-  alias Scam.AddictionCheck.Conversation
   alias Scam.AddictionCheck.ConversationPart
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, stream(socket, :conversations, AddictionCheck.list_conversations())}
+    {:ok, stream(socket, :conversations, [])}
   end
 
   @impl true
   def handle_params(params, _url, socket) do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  end
-
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Conversation")
-    |> assign(:conversation, AddictionCheck.get_conversation!(id))
-  end
-
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Conversation")
-    |> assign(:conversation, %Conversation{})
   end
 
   defp apply_action(socket, :index, _params) do
@@ -33,18 +20,7 @@ defmodule ScamWeb.ConversationLive.Index do
     |> assign(:conversation, nil)
   end
 
-  @impl true
-  def handle_info({ScamWeb.ConversationLive.FormComponent, {:saved, conversation}}, socket) do
-    {:noreply, stream_insert(socket, :conversations, conversation)}
-  end
 
-  @impl true
-  def handle_event("delete", %{"id" => id}, socket) do
-    conversation = AddictionCheck.get_conversation!(id)
-    {:ok, _} = AddictionCheck.delete_conversation(conversation)
-
-    {:noreply, stream_delete(socket, :conversations, conversation)}
-  end
 
   @impl true
   def handle_event("client_message", %{"client_id" => client_id, "message" => message}, socket) do
@@ -75,15 +51,7 @@ defmodule ScamWeb.ConversationLive.Index do
 
     conversation_parts_with_chatbot = AddictionCheck.list_conversation_parts(%{ client_id: client_id })
 
-    {:noreply, stream_update(socket, :conversations, conversation_parts_with_chatbot)}
-  end
-
-  @impl true
-  def handle_event("create_conv", %{"client_id" => client_id}, socket) do
-    conversation = %Conversation{client_id: client_id, questions: [], answers: []}
-    {:ok, _} = AddictionCheck.create_conversation(conversation)
-
-    {:noreply, stream_insert(socket, :conversations, conversation)}
+    {:noreply, stream_insert(socket, :conversations, conversation_parts_with_chatbot)}
   end
 
 #  Should create new user
